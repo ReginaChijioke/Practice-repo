96b1af005764a7950a141bdd2573c009e7d900a9
ReginaChijioke <ginakosy250@gmail.com>
2026-09-03 00:13:48 +0100

    my intent_chatbot project

---
+++ Intent_chatbot/intents.py
@@ -0,0 +1,7 @@
+INTENT_MAP = {
+    "greetings" : ["hi","hello","hey"],
+    "ask_name" : ["what is your name", "who are you"],
+    "ask_time" : ["what is the time", "tell the time"],
+    "joke" : ["tell me a joke", "make me laugh"],
+    "goodbye" : ["bye", "see you", "goodbye"]
+}
