a3347aeb71fbe299dace0b42db8f50e32b814c4c
ReginaChijioke <ginakosy250@gmail.com>
2026-09-03 23:10:52 +0100

    my Intent_chatbot

--- Intent_chatbot/nlp_utils.py
+++ Intent_chatbot/nlp_utils.py
@@ -1,4 +1,5 @@
 import string 
+from intents import INTENT_MAP
 def clean_input(raw_text):
     low = raw_text.lower()
     clean = low.strip()
@@ -7,3 +8,12 @@ def clean_input(raw_text):
     return cleaned_text
 print(clean_input("  kosy,, "))
 
+
+def match_intent(cleaned_text, intent_map):
+    for intent, phrase_list in intent_map.items():
+        for phrase in phrase_list:
+            if phrase in cleaned_text:
+                return intent
+
+    return None
+print(match_intent("hey", INTENT_MAP))
