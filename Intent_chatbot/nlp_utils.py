1a734e40741dd79684a2ce13a3a522136489e57e
ReginaChijioke <ginakosy250@gmail.com>
2026-09-03 22:15:00 +0100

    my Intent_chatbot

--- Intent_chatbot/nlp_utils.py
+++ Intent_chatbot/nlp_utils.py
@@ -1,8 +1,9 @@
+import string 
 def clean_input(raw_text):
     low = raw_text.lower()
     clean = low.strip()
-    
-
-
-
+    punctuation = clean.maketrans("", "", string.punctuation)
+    cleaned_text = clean.translate(punctuation)
+    return cleaned_text
+print(clean_input("  kosy,, "))
 
