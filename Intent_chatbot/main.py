6dfb6bd59ed66a52b718bc473836c2da5796e285
ReginaChijioke <ginakosy250@gmail.com>
2026-09-04 22:39:15 +0100

    my Intent_chatbot

--- Intent_chatbot/main.py
+++ Intent_chatbot/main.py
@@ -1,7 +1,13 @@
-import clean_input, match_intent from nlp_utils
-import get_response from responses 
-import INTENT_MAP from intents
-import RESPONSE_MAP from responses 
+from nlp_utils import clean_input, match_intent
+from responses import get_response, RESPONSE_MAP
+from intents import INTENT_MAP
 
-exit_phrases = {"exit", "bye", "thamk you"}
-exchange_count = 0
+exit_phrases = {"exit", "bye", "quit"}
+
+while True:
+    raw_input = input("You: ")
+    cleaned = clean_input(raw_input)
+    if cleaned in exit_phrases:
+        print("see ya")
+        break
+
