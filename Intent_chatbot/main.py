be95c0534071c7daa3f18a38e92f7b130dd37168
ReginaChijioke <ginakosy250@gmail.com>
2026-09-07 09:17:57 +0100

    my Intent_chatbot

--- Intent_chatbot/main.py
+++ Intent_chatbot/main.py
@@ -3,11 +3,17 @@ from responses import get_response, RESPONSE_MAP
 from intents import INTENT_MAP
 
 exit_phrases = {"exit", "bye", "quit"}
-
+exchange_count = 0
 while True:
     raw_input = input("You: ")
     cleaned = clean_input(raw_input)
     if cleaned in exit_phrases:
         print("see ya")
         break
+
+    matched_intent = match_intent(clean_input, INTENT_MAP)
+    bot_response = get_response(matched_intent, RESPONSE_MAP)
+    print("Bot:", bot_response)
+    exchange_count += 1
+print(exchange_count)
 
